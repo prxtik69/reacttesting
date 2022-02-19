d40b3c5b0e037d72067a667390e556b7d1695dcb
prxtik69 <pratikdeshpande007pvd@gmail.com>
2022-02-19 23:12:48 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const para = document.getElementById("para");
+  localStorage.setItem("Email", "pratik@pratik.com");
+  localStorage.setItem("Pass", "Pratik");
+
+  const returnHash = () => {
+    let hashmap = new Map([
+      ["Email", localStorage.getItem("Email")],
+      ["Pass", localStorage.getItem("Pass")],
+    ]);
+    return hashmap;
+  };
+
+  function handleLogin() {
+    let loginEmail = document.getElementById("typeEmailX").value;
+    let loginPass = document.getElementById("typePasswordX").value;
+
+    if (
+      loginEmail === localStorage.getItem("Email") &&
+      loginPass === localStorage.getItem("Pass")
+    ) {
+      para.innerHTML = "Login Successful";
+    } else {
+      para.innerHTML = "Login Failed";
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="vh-100 gradient-custom">
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div className="card bg-dark text-white">
+              <div className="card-body p-5 text-center">
+                <div className="mb-md-5 mt-md-4 pb-5">
+                  <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                  <p className="text-white-50 mb-5">
+                    Please enter your login and password!
+                  </p>
+
+                  <div className="form-outline form-white mb-4">
+                    <input
+                      type="email"
+                      id="typeEmailX"
+                      className="form-control form-control-lg"
+                    />
+                    <label className="form-label" htmlFor="typeEmailX">
+                      Email
+                    </label>
+                  </div>
+
+                  <div className="form-outline form-white mb-4">
+                    <input
+                      type="password"
+                      id="typePasswordX"
+                      className="form-control form-control-lg"
+                    />
+                    <label className="form-label" htmlFor="typePasswordX">
+                      Password
+                    </label>
+                  </div>
+
+                  <p className="small mb-5 pb-lg-2">
+                    <a className="text-white-50" href="#!">
+                      Forgot password?
+                    </a>
+                  </p>
+
+                  <button
+                    className="btn btn-outline-light btn-lg px-5"
+                    type="submit"
+                    onClick={handleLogin}
+                  >
+                    Login
+                  </button>
+
+                  <p id="para"></p>
+
+                  <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                    <a href="#!" className="text-white">
+                      <i className="fab fa-facebook-f fa-lg"></i>
+                    </a>
+                    <a href="#!" className="text-white">
+                      <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
+                    </a>
+                    <a href="#!" className="text-white">
+                      <i className="fab fa-google fa-lg"></i>
+                    </a>
+                  </div>
+                </div>
+
+                <div>
+                  <p className="mb-0">
+                    Don't have an account?{" "}
+                    <a href="#!" className="text-white-50 fw-bold">
+                      Sign Up
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
